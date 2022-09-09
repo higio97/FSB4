10874a4972a601a36537c5aa361194db26fbad45
higio97 <92028330+higio97@users.noreply.github.com>
2022-09-09 21:15:55 +0700

    Update heroku.py

--- plugins/heroku.py
+++ plugins/heroku.py
@@ -1,15 +1,4 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-# Ported by @davialfajr
-# https://t.me/anonymousmyboo
-#
+# (©) Anonymous
 
 import os
 import socket
